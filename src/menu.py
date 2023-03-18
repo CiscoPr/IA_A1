3614fbf878fd21fa9d9cc44138ff28dd7bc875fe
Francisco Prada <fmamp24@gmail.com>
2023-03-18 20:16:26 +0000

    generates map almost correctly

--- src/menu.py
+++ src/menu.py
@@ -1,5 +1,6 @@
 import pygame
-
+import os
+from level import *
 pygame.init()
 
 # Set up the window
@@ -30,6 +31,86 @@ title_surface = title_font.render("Space Block", True, (255, 255, 255))
 title_rect = title_surface.get_rect(center=(screen.get_width() // 2, 150))
 
 
+def level_selector():
+    number_of_levels = 0
+    maps_folder = "../src/maps"
+
+    for level in os.listdir(maps_folder):
+        if os.path.isfile(os.path.join(maps_folder, level)):
+            number_of_levels += 1
+
+    print("Number of levels: ", number_of_levels)
+
+    options = []
+    option_rects = []
+    for i in range(1, number_of_levels+1):
+        options.append("Level " + str(i))
+
+    options.append("Return")
+    done3 = False
+    selected_option = 0
+
+    while not done3:
+
+        title_surface = title_font.render("Level selector", True, (255,255,255))
+        title_rect = title_surface.get_rect(center=(screen.get_width() // 2, 150))
+        # Handle events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    selected_option -= 1
+                    if selected_option < 0:
+                        selected_option = len(options) - 1
+                elif event.key == pygame.K_DOWN:
+                    selected_option += 1
+                    if selected_option >= len(options):
+                        selected_option = 0
+                elif event.key == pygame.K_RETURN:
+                    is_selected = True
+                    if options[selected_option] == "Return":
+                        done3 = True
+                    else:
+                        map_generator(selected_option + 1)
+                    print("Selected option:", options[selected_option])
+
+
+
+        # Clear the screen
+        screen.blit(background, (0, 0))
+        # Draw title
+        screen.blit(title_surface, title_rect)
+
+        for i, option in enumerate(options):
+            option_surface = font.render(option, True, (255, 255, 255))
+            option_rect = option_surface.get_rect(center=(screen.get_width() // 2, 300 + i * 50))
+            option_rects.append(option_rect)
+            screen.blit(option_surface, option_rect)
+
+            # Check if Enter button is pressed and change pointer size
+        if pygame.key.get_pressed()[pygame.K_RETURN]:
+            pointer_size = font.size("> ")[1] + 10
+        else:
+            pointer_size = font.size("> ")[1]
+        # Draw pointer
+        pointer_rect = pygame.Rect(pointer_x, option_rects[selected_option].centery - pointer_size // 2, pointer_size, pointer_size)
+
+        pygame.draw.rect(screen, (255, 255, 255), pointer_rect)
+        #if is_selected:
+        #    pygame.draw.rect(screen, (255, 255, 255), pointer_rect)
+        #else:
+
+
+        # Flip the display
+        pygame.display.flip()
+
+        # Wait for a bit to control the animation speed
+        pygame.time.wait(20)
+
+
+
+
 def mode_selector():
     options = ["Player Mode", "AI Mode", "Return"]
     option_rects = []
@@ -58,6 +139,10 @@ def mode_selector():
                     is_selected = True
                     if options[selected_option] == "Return":
                         done2 = True
+                    elif options[selected_option] == "Player Mode":
+                        level_selector()
+                    elif options[selected_option] == "AI Mode":
+                        level_selector()
                     print("Selected option:", options[selected_option])
 
 
